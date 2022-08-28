ac58a0bf0c62cac9e2782055c3220aa03507ce2b
Ned Hunter <eehuntercats@gmail.com>
2022-08-28 18:49:00 -0400

    This is a major improvement for loot tables.

--- src/main/kotlin/com/oyosite/ticon/util/datagen/TerraBlockLootTableGenerator.kt
+++ src/main/kotlin/com/oyosite/ticon/util/datagen/TerraBlockLootTableGenerator.kt
@@ -3,60 +3,58 @@ package com.oyosite.ticon.util.datagen
 import com.oyosite.ticon.block.BlockRegistry
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricLootTableProvider
 import net.minecraft.block.Block
 import net.minecraft.item.ItemConvertible
 import net.minecraft.item.Items.RAW_COPPER
-import net.minecraft.loot.LootTable
 import net.minecraft.loot.LootTable.Builder
 import java.util.function.Function
-import kotlin.reflect.KFunction
 
 class TerraBlockLootTableGenerator(dataGenerator: FabricDataGenerator): FabricBlockLootTableProvider(dataGenerator) {
+
+
     override fun generateBlockLootTables() = BlockRegistry.run{
-        addDrops(
-            TERRATHIL_STONE.silkable, TERRATHIL_COBBLESTONE to TERRATHIL_STONE,
-            TERRATHIL_COPPER_ORE.silkable, RAW_COPPER to TERRATHIL_COPPER_ORE,
 
-            TERRA_LOG, TERRA_PLANKS,
-            TERRA_LEAVES.silkable, //TODO: Terra leaves standard drops
+        TERRATHIL_STONE.silkable drops TERRATHIL_COBBLESTONE
+        TERRATHIL_COPPER_ORE.silkable drops RAW_COPPER
 
-        )
+        TERRA_LOG drops ITSELF
+        TERRA_PLANKS drops ITSELF
+        TERRA_LEAVES drops leaves(TERRA_SAPLING, SAPLING_DROP_CHANCE)
 
-        /*addDropWithSilkTouch(TERRATHIL_STONE)
-        addDrop(TERRATHIL_STONE, TERRATHIL_COBBLESTONE)
-
-        addDropWithSilkTouch(TERRATHIL_COPPER_ORE)
-        addDrop(TERRATHIL_COPPER_ORE, RAW_COPPER)
-
-
-        addDrop(TERRA_LOG)*/
-
-
+        
     }
 
 
 
-    
+    private infix fun Block.drops(self: ITSELF) = +this
+    private infix fun Block.drops(item: ItemConvertible) = +(this to item)
+    private infix fun Block.drops(lootTable: Builder) = +(this to lootTable)
+    private infix fun Block.drops(lootTableSupplier: (Block)->Builder) = +(this to lootTableSupplier)
+    private infix fun Silkable.drops(item: ItemConvertible) = this.block.let{addDropWithSilkTouch(it);+(it to item)}
+    private infix fun Silkable.drops(lootTable: Builder) = this.block.let{addDropWithSilkTouch(it);+(it to lootTable)}
+    private infix fun Silkable.drops(lootTableSupplier: (Block)->Builder) = this.block.let{addDropWithSilkTouch(it);+(it to lootTableSupplier)}
 
+
+    @JvmName("varargLeaves")
+    private fun leaves(sapling: Block, vararg dropChance: Float): (Block)->Builder = { leavesDrop(it, sapling, *dropChance) }
+    private fun leaves(sapling: Block, dropChance: FloatArray): (Block)->Builder = { leavesDrop(it, sapling, *dropChance) }
+
+    private operator fun Block.unaryPlus() = addDrop(this)
+    private operator fun Silkable.unaryPlus() = addDropWithSilkTouch(block)
+
+    @JvmName("addItemLoot")
+    operator fun Pair<Block, ItemConvertible>.unaryPlus() = addDrop(first, second)
+    @JvmName("addLootTable")
+    operator fun Pair<Block, Builder>.unaryPlus() = addDrop(first, second)
+    @JvmName("addLootTableSupplier")
+    operator fun Pair<Block, (Block)->Builder>.unaryPlus() = addDrop(first, Function(second))
 
     @JvmInline private value class Silkable(val block: Block)
     private val Block.silkable get() = Silkable(this)
-    private fun addDrops(vararg blocks: Any) {
-        for (block in blocks) when (block) {
-            is Block -> addDrop(block)
-            is Pair<*, *> -> addDrop(block)
-            is Silkable -> addDropWithSilkTouch(block.block)
-        }
-    }
-    private fun addDrop(data: Pair<*,*>){
-        if (data.second !is Block) return if (data.second is Silkable) addDropWithSilkTouch((data.second as Silkable).block, data.first as Block) else Unit
 
-        val block: Block = data.second as Block
-        when(val drop: Any = data.first!!){
-            is ItemConvertible -> addDrop(block, drop)
-            is Builder -> addDrop(block, data.first as Builder)
-            else -> addDrop(block, Function{ (data.first as (Block)->Builder?).invoke(it) } )
-        }
+
+    companion object{
+        private val SAPLING_DROP_CHANCE = floatArrayOf(0.05f, 0.0625f, 0.083333336f, 0.1f)
+        private object ITSELF
     }
 }
